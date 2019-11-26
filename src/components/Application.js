d894ee183eb21d331999ba007250af440744b1c2
Chris <chris.mastel@gmail.com>
2019-11-26 11:07:52 -0700

    add appointments to schedule, and render

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
 import "components/Application.scss";
+import Appointment from "components/Appointment";
 import DayList from './DayList'
 
-
+// ---------------------- GLOBAL VARIABLES -------------- //
 const days = [
   {
     id: 1,
@@ -22,8 +23,66 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Santa Claus",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Jabba the Hutt",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+];
+
+
+// ------------------------- APPLICATION FUNCTION -------------------- //
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  
+  const renderSchedule = appointments.map(appointment =>
+    <Appointment
+      key={props.id}
+      {...appointment}
+
+    />
+    )
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -47,7 +106,9 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {renderSchedule}
+
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
