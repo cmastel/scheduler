cbdef967769785ecb3d824408ef2a8d3f44ed015
Chris <chris.mastel@gmail.com>
2019-11-27 10:24:47 -0700

    add Custom Hook for transitions

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial])
+
+  function transition(next, replace = false) {
+    if (replace === true) {
+      history.pop();
+    }
+    history.push(next);
+    setMode(() => next);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      const prevMode = history[history.length - 1]
+      setMode(() => prevMode);
+    }
+  }
+
+  return { mode, transition, back };
+};
