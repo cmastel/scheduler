069c176cf19824c449072f2eef7df1c9de1e0183
Chris <chris.mastel@gmail.com>
2019-12-03 14:46:43 -0700

    implement Cypress end-to-end testing

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -10,38 +10,6 @@ if (process.env.NODE_ENV !== 'test'){
 }
 
 export default function useApplicationData() {
-  
-  // const SET_DAY = "SET_DAY";
-  // const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-  // const SET_INTERVIEW = "SET_INTERVIEW";
-
-  // function reducer(state, action) {
-  //   switch (action.type) {
-  //     case SET_DAY:
-  //       return { 
-  //         ...state, 
-  //         day: action.day 
-  //       }
-  //     case SET_APPLICATION_DATA:
-  //       return { 
-  //         ...state, 
-  //         days: action.days,
-  //         appointments: action.appointments,
-  //         interviewers: action.interviewers
-  //       }
-  //     case SET_INTERVIEW:
-  //       return {
-  //         ...state,
-  //         appointments: action.appointments,
-  //         days: action.newDays,
-  //        }
-  //     default:
-  //       throw new Error(
-  //         `Tried to reduce with unsupported action type: ${action.type}`
-  //       );
-  //   }
-  // }
-
   const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
---
+++ src/reducers/application.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from './application'
+
+import { render, 
+  cleanup, 
+ } from "@testing-library/react";
+
+
+afterEach(cleanup);
+
+describe("Application Reducer", () => {
+  it("throws an error with an unsupported type", () => {
+    expect(() => reducer({}, {type: null })).toThrowError(
+      /tried to reduce with unsupported action type/i
+    );
+  });
+});
